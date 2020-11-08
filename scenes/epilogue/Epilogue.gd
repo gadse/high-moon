7f64bd0af9216a92f65ec68c1eede9b3bd37f69e
Thomas Bittner <thomas.bittner@posteo.de>
2020-11-08 11:35:18 +0100

    Clean up Epilogue

--- scenes/epilogue/Epilogue.gd
+++ scenes/epilogue/Epilogue.gd
@@ -1,7 +1,5 @@
 extends Node
 
-signal finished
-
 var messages = {
 	"byId": {},
 	"allIds": []
@@ -28,24 +26,19 @@ func _on_next_screen_requested():
 	if self._is_next_screen_available():
 		self._switch_to_next_screen()
 	else:
-		emit_signal("finished")
+		queue_free()
 
 func _is_next_screen_available():
 	return index_of_current_screen < messages["allIds"].size()
 	
 func _switch_to_next_screen():
-	self._delete_old_screen()
 	self._show_new_screen()
 	index_of_current_screen += 1
-
-func _delete_old_screen():
-	if current_screen:
-		self.remove_child(current_screen)
 
 func _show_new_screen():
 	var screen_id = messages["allIds"][index_of_current_screen]
 	
 	current_screen = FadingScene.instance()
-	current_screen.connect("faded_out", self, "_on_next_screen_requested")
+	current_screen.connect("tree_exited", self, "_on_next_screen_requested")
 	current_screen.caption = messages["byId"][screen_id]["text"]
 	self.add_child(current_screen)
