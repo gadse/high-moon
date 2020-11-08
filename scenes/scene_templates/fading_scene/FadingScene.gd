a4403516c1ce3757b7c617b747f3f0e86087d0b0
Thomas Bittner <thomas.bittner@posteo.de>
2020-11-08 11:32:07 +0100

    Fade out on mouse click

--- scenes/scene_templates/fading_scene/FadingScene.gd
+++ scenes/scene_templates/fading_scene/FadingScene.gd
@@ -1,6 +1,4 @@
-extends MarginContainer
-
-signal faded_out
+extends TextureRect
 
 export(String, FILE) var background_image
 export(String, MULTILINE) var caption
@@ -10,7 +8,7 @@ var is_fading_in = true
 
 func _ready():
 	if background_image:
-		$BackgroundImage.texture = load(background_image)
+		self.texture = load(background_image)
 	$Caption.bbcode_text = caption
 	$FadeOutTimer.wait_time = fade_out_timeout
 	
@@ -21,7 +19,15 @@ func _on_FadeAnimationPlayer_animation_finished(_animation_name):
 		is_fading_in = false
 		$FadeOutTimer.start()
 	else:
-		emit_signal("faded_out")
+		queue_free()
 
 func _on_FadeOutTimer_timeout():
+	self._fade_out()
+
+func _on_FadeBlackRect_gui_input(event):
+	if event.is_pressed() and event.button_index == BUTTON_LEFT:
+		self._fade_out()
+
+func _fade_out():
+	$FadeOutTimer.stop()
 	$FadeAnimationPlayer.play_backwards("FadeIn")
