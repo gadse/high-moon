68fead68de038637d880135b5a61272ca87ad8e1
Thomas Bittner <thomas.bittner@posteo.de>
2020-11-09 18:46:29 +0100

    Add Credits scene
    - Credits scene has fade in/out effect
    - Music from main menu keeps playing
    - Clicking on the credits returns to the main menu

--- scenes/kill_scene/KillScene.gd
+++ scenes/kill_scene/KillScene.gd
@@ -1,8 +1,16 @@
 extends TextureRect
 
 func _ready():
-	$GunShot.connect("finished", self, "queue_free")
-	$itsHighMoon.play()
+	$Fader.connect("faded_in", self, "_play_it_is_high_moon")
+	$Fader.connect("faded_out", self, "queue_free")
 
-func _on_GunShotTimer_timeout():
+	$ItsHighMoon.connect("finished", self, "_play_gunshot")
+	$GunShot.connect("finished", $Fader, "fade_out")
+
+	$Fader.fade_in()
+
+func _play_it_is_high_moon():
+	$ItsHighMoon.play()
+
+func _play_gunshot():
 	$GunShot.play()
