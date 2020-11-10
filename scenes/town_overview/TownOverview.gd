302f1edb41c159a9dad8e16b6cee5b7437944da8
PixelMaster98 <misteradi126@gmail.com>
2020-11-10 01:40:07 +0100

    Basic experimental layout for town overview
    Includes clickable houses, world-class artwork, fancy labels, pop
    culture music references and the blood and sweat I spent on figuring out
    WHY THE FUCK my mouse clicks weren't getting through

--- scenes/town_overview/TownOverview.gd
+++ scenes/town_overview/TownOverview.gd
@@ -1,7 +1,15 @@
-extends ColorRect
+extends Node2D
 
 func _ready():
-	$Fader.connect("faded_in", $Fader, "fade_out")
+	$Fader.connect("faded_in", $Fader, "init")
 	$Fader.connect("faded_out", self, "queue_free")
 
 	$Fader.fade_in()
+
+func init():
+	pass
+
+
+func _on_Experimental_FadeOut_button_up():
+	$Fader.fade_out()
+	pass # Replace with function body.
