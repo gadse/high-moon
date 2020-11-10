f9e895867f0597dced31249f34ffd96c0a95054b
PixelMaster98 <misteradi126@gmail.com>
2020-11-10 01:46:08 +0100

    Renamed script and removed excessive debug logging

--- scenes/town_overview/House.gd
+++ scenes/town_overview/House.gd
@@ -8,7 +8,6 @@ export var house :String
 #			get_node("../Info").set_text(house + " house clicked")
 
 func _on_input_event(viewport, event, shape_idx):
-	print("event received")
 	if event is InputEventMouseButton:
 		if event.is_pressed() and event.button_index == BUTTON_LEFT:
 			get_node("../Info").set_text(house + " house clicked")
