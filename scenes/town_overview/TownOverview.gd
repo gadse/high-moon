4ef31c830060ef6fb5ae0109b4e7119677d54a7f
Peter Bozsoky <peter-bozsoky@posteo.de>
2020-11-23 18:10:10 +0100

    Town Overview: Remove story/twison references

--- scenes/town_overview/TownOverview.gd
+++ scenes/town_overview/TownOverview.gd
@@ -1,54 +1,11 @@
 extends TextureRect
 
-var TwisonHelper = preload("res://modules/twison-godot/twison_helper.gd")
 var Dialog = preload("res://scenes/templates/dialog/Dialog.tscn")
-var story = TwisonHelper.new()
 
-const number_keys = [
-	KEY_0,
-	KEY_1,
-	KEY_2,
-	KEY_3,
-	KEY_4,
-	KEY_5,
-	KEY_6,
-	KEY_7,
-	KEY_8,
-	KEY_9,
-	KEY_0
-]
-
-var current_passage = null
-var just_starting = false
 
 func _ready():
 	#$Fader.connect("faded_out", self, "queue_free")
 	$Fader.fade_in()
-
-	story.parse_file("examples/example_story.json")
-	current_passage = story.start()
-	$Info.text = story.get_story_name() + "\n" + current_passage.text
-	_output_links(current_passage)
-
-func _input(event):
-	if event is InputEventMouseButton and \
-			event.is_pressed() and \
-			event.button_index == BUTTON_LEFT:
-		print("registered left click")
-		if story.is_finished():
-			print("FINISHED")
-			$Info.text = current_passage.text + "\n"
-			#$Fader.fade_out()
-		else:
-			story.traverse(0)
-			$Info.text = current_passage.text + "\n"
-			_output_links(current_passage)
-
-func _output_links(passage):
-	var ix: int = 0
-	for link in passage.links:
-		$Info.text += "\n"
-		$Info.text += String(ix) + "> " + link.name
 
 func _on_Experimental_FadeOut_button_up():
 	$Fader.connect("faded_out", self, "queue_free")
